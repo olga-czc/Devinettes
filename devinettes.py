dd6ad55b306a65d6d0b93abb28b44295d04479d5
Olga Cazacioc <1697327@cstjean.qc.ca>
2020-10-22 14:20:48 -0400

    Validation d’entrées

--- devinettes.py
+++ devinettes.py
@@ -4,13 +4,67 @@ Jeu de devinettes.
 
 Par olga Cazacioc
 """
+import random
 
 
 def main():
     """
     Fonction principale
     """
-    pass
+    print("Bonjour, je m'appele Olax 2020 \nJ'ai choisi un nombre entier entre 1 et 100"
+         "\nPouvez-vous le deviner?")
+
+    nombre_aleatoire= random.randrange(101)
+    trouver_nombre(nombre_aleatoire)
+
+
+def trouver_nombre(nombre_aleatoire):
+    """
+    Affiche progressivement une séquence de nombre à partir de 1 en montant
+
+    :param nombre_aleatoire: jusqu'à ou compter
+    """
+    nrEssai = 1
+    chiffre = 0
+
+    while chiffre != nombre_aleatoire:
+
+        chiffre = lire_chiffre(nrEssai)
+
+        nrEssai += 1
+
+
+
+        if chiffre > nombre_aleatoire:
+         print("> Votre nombre est trop grand...")
+         verifier_nb_essai(nrEssai, nombre_aleatoire)
+
+        else:
+         print("> Votre nombre est trop petit...")
+         verifier_nb_essai(nrEssai, nombre_aleatoire)
+
+
+
+    print("Bravo, vous avez deviné le nombre")
+
+
+def  lire_chiffre(nr):
+    while True:
+        try:
+            print("Essai ", nr, ": ", end='')
+            chiffre = int(input())
+        except ValueError:
+            print(">>> ERREUR: Entrez un nombre entier svp")
+            continue
+        else:
+            return chiffre
+            break
+
+def verifier_nb_essai(nr, nr_aleatoire):
+    if nr == 11:
+        print("Désolé, vous avez échoué après 10 tentatives")
+        #print("Le nombre choisi était: ", nr_aleatoire)
+
 
 
 if __name__ == '__main__':
