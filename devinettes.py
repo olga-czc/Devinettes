c98e19420879ff68841b4fe47373fccb324e76c8
Olga Cazacioc <1697327@cstjean.qc.ca>
2020-10-22 10:17:26 -0400

    Initial commit

---
+++ devinettes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Jeu de devinettes.
+
+Par olga Cazacioc
+"""
+
+
+def main():
+    """
+    Fonction principale
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
