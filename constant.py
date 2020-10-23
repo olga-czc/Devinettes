de8af9b9c59a75e54627d082b6223f42d7bf4e07
Olga Cazacioc <1697327@cstjean.qc.ca>
2020-10-22 23:48:44 -0400

    Assurance qualité

---
+++ constant.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Jeu de devinettes.
+
+Par olga Cazacioc
+"""
+
+# Constantes
+MAX_ESSAIS = 10
--- devinettes.py
+++ devinettes.py
@@ -5,6 +5,7 @@ Jeu de devinettes.
 Par olga Cazacioc
 """
 import random
+import constant
 
 
 def main():
@@ -12,52 +13,67 @@ def main():
     Fonction principale
     """
     print("Bonjour, je m'appele Olax 2020 \nJ'ai choisi un nombre entier entre 1 et 100"
-         "\nPouvez-vous le deviner?")
+          "\nPouvez-vous le deviner?")
 
-    nombre_aleatoire= random.randrange(101)
+    nombre_aleatoire = random.randrange(101)
     sequence = []
-    tentatives = 10
-    tent = 0
+
+    tentatives = 0
     nb_parties_jouées = 0
     nb_essais_effectués = 0
-    nb_essais = 0
 
-    trouver_nombre(nombre_aleatoire, sequence, tentatives, tent, nb_parties_jouées, nb_essais_effectués)
+    trouver_nombre(nombre_aleatoire, sequence, tentatives, nb_parties_jouées, nb_essais_effectués)
 
 
-def trouver_nombre(nombre_aleatoire, sequence, tentatives, tent, nb_parties_jouées, nb_essais_effectués):
+def trouver_nombre(nombre_aleatoire, sequence, tentatives, nb_parties_jouées, nb_essais_effectués):
     """
-    Affiche progressivement une séquence de nombre à partir de 1 en montant
+    Compare le nombre donné avec le nombre généré
 
-    :param nombre_aleatoire: jusqu'à ou compter
+    :param nombre_aleatoire:
+    :param sequence:
+    :param tentatives:
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
     """
-    nrEssai = 1
+
+    tentatives += 1
     chiffre = 0
 
     while chiffre != nombre_aleatoire:
 
-        chiffre = lire_chiffre(nrEssai, sequence, nombre_aleatoire, nb_parties_jouées, nb_essais_effectués)
+        chiffre = lire_chiffre(tentatives, sequence, nombre_aleatoire, nb_parties_jouées, nb_essais_effectués)
         sequence.append(chiffre)
-        nrEssai += 1
+        tentatives += 1
         nb_essais_effectués += 1
 
         if chiffre > nombre_aleatoire:
-         print("> Votre nombre est trop grand...")
-         verifier_nb_essai(nrEssai, nombre_aleatoire, tentatives, nb_parties_jouées, nb_essais_effectués)
+            print("> Votre nombre est trop grand...")
+            verifier_nb_essai(nombre_aleatoire, tentatives, nb_parties_jouées, nb_essais_effectués)
 
         elif chiffre < nombre_aleatoire:
-         print("> Votre nombre est trop petit...")
-         verifier_nb_essai(nrEssai, nombre_aleatoire, tentatives, nb_parties_jouées, nb_essais_effectués)
+            print("> Votre nombre est trop petit...")
+            verifier_nb_essai(nombre_aleatoire, tentatives, nb_parties_jouées, nb_essais_effectués)
 
     gagner(sequence, nb_parties_jouées, nb_essais_effectués)
 
-def  lire_chiffre(nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effectués):
+
+def lire_chiffre(nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effectués):
+    """
+    Verifie le nombre donné
+
+    :param nr:
+    :param sequence:
+    :param nr_aleatoire:
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
+    :return:
+    """
     while True:
         try:
             print("Essai ", nr, ": ", end='')
             # chiffre = int(input())
             ligne = input()
-            verifierLigne(ligne, nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effectués)
+            verifier_ligne(ligne, nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effectués)
 
             chiffre = int(ligne)
         except ValueError:
@@ -65,57 +81,97 @@ def  lire_chiffre(nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effe
             continue
         else:
             return chiffre
-            break
 
-def verifier_nb_essai(nr, nr_aleatoire, tentatives, nb_parties_jouées, nb_essais_effectués):
-    if nr == 11:
-        print("> Désolé, vous avez échoué après ", tentatives, "tentatives")
+
+def verifier_nb_essai(nr_aleatoire, tentatives, nb_parties_jouées, nb_essais_effectués):
+    """
+    Verifie si le nombre d'essais ne depasse pas le nombre maximal d'essais
+
+    :param nr_aleatoire:
+    :param tentatives:
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
+    """
+    if tentatives == constant.MAX_ESSAIS + 1:
+        print("> Désolé, vous avez échoué après ", constant.MAX_ESSAIS, "tentatives")
         print("> Le nombre choisi était: ", nr_aleatoire)
         recommencer(nb_parties_jouées, nb_essais_effectués)
 
-def  gagner(sequence, nb_parties_jouées, nb_essais_effectués):
+
+def gagner(sequence, nb_parties_jouées, nb_essais_effectués):
+    """
+    Affiche le résultat si gagné
+
+    :param sequence:
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
+    """
     print("> Bravo, vous avez deviné le nombre")
     print("> Votre séquence gagnante est:", sequence)
     recommencer(nb_parties_jouées, nb_essais_effectués)
 
-def echouer(nr, nr_aleatoire,nb_parties_jouées, nb_essais_effectués):
+
+def echouer(nr, nr_aleatoire, nb_parties_jouées, nb_essais_effectués):
+    """
+    Affiche le résultat si echoué
+
+    :param nr:
+    :param nr_aleatoire:
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
+    """
     print("> Désolé, vous avez échoué après ", nr, "tentatives")
     print("> Le nombre choisi était: ", nr_aleatoire)
     recommencer(nb_parties_jouées, nb_essais_effectués)
 
-def recommencer(nb_parties_jouées, nb_essais_effectués):
 
+def recommencer(nb_parties_jouées, nb_essais_effectués):
+    """
+    Reset du jeu ou fin
+
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
+    """
     while True:
 
         print("Voulez-vous rejouer? [O/N] ", end='')
         reponse = input()
         print("")
-        nb_parties_jouées +=1
+        nb_parties_jouées += 1
         if reponse == 'O' or reponse == 'o' or reponse == 'oui':
             nombre_aleatoire = random.randrange(101)
             sequence = []
             tentatives = 10
-            tent = 0
-            trouver_nombre(nombre_aleatoire, sequence, tentatives, tent, nb_parties_jouées, nb_essais_effectués)
+            trouver_nombre(nombre_aleatoire, sequence, tentatives, nb_parties_jouées, nb_essais_effectués)
         elif reponse == 'N' or reponse == 'n' or reponse == 'non':
             print("   Nombre de parties jouées: ", nb_parties_jouées)
-            print("  Nombre d'essais effectués: ",nb_essais_effectués)
-            print("Moyenne d'essais par partie: ",nb_essais_effectués/nb_parties_jouées)
+            print("  Nombre d'essais effectués: ", nb_essais_effectués)
+            print("Moyenne d'essais par partie: ", nb_essais_effectués / nb_parties_jouées)
             print("Au revoir !")
             break
         else:
             print("Choix invalide")
 
-def verifierLigne(ligne, nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effectués):
+
+def verifier_ligne(ligne, nr, sequence, nr_aleatoire, nb_parties_jouées, nb_essais_effectués):
+    """
+    Verifie le cheat code
+
+    :param ligne: input d'utilisateur
+    :param nr:
+    :param sequence:
+    :param nr_aleatoire:
+    :param nb_parties_jouées:
+    :param nb_essais_effectués:
+    """
     if ligne == 'PPP':
-       nb_essais_effectués += 1
-       echouer(nr, nr_aleatoire, nb_parties_jouées, nb_essais_effectués)
+        nb_essais_effectués += 1
+        echouer(nr, nr_aleatoire, nb_parties_jouées, nb_essais_effectués)
     if ligne == 'GGG':
-       nb_essais_effectués += 1
-       sequence.append(ligne)
-       gagner(sequence, nb_parties_jouées, nb_essais_effectués)
+        nb_essais_effectués += 1
+        sequence.append(ligne)
+        gagner(sequence, nb_parties_jouées, nb_essais_effectués)
+
 
 if __name__ == '__main__':
     main()
-
-
